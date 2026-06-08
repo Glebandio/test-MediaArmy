f92a1a2df428bfba1d24142709bdd0d7bfc7430d
catln_space <oraclgleb@gmail.com>
2026-06-08 17:40:49 +0500

    рдлорлорло

---
+++ main.js
@@ -0,0 +1,34 @@
+const openModalBtn = document.getElementById('modal');
+const modalOverlay = document.getElementById('modalOverlay');
+const modal = document.querySelector('.modal');
+
+function openModal(buttonId) {
+    modalOverlay.classList.add('active');
+    setTimeout(() => {
+        modal.classList.add('modal_show');
+    }, 10);
+}
+
+function closeModal() {
+    modal.classList.remove('modal_show');
+    setTimeout(() => {
+        modalOverlay.classList.remove('active');
+    }, 300);
+}
+
+openModalBtn.addEventListener('click', function(event) {
+    const buttonId = event.currentTarget.id;
+    openModal(buttonId);
+});
+
+modalOverlay.addEventListener('click', function(event) {
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && modalOverlay.classList.contains('active')) {
+        closeModal();
+    }
+});
